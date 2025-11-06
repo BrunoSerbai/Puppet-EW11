92287b7681a9a237d8260437500b65e629dc0cee
Bruno Serbai <brunoserbai@outlook.com>
2025-11-06 16:37:41 -0300

    udpate puppet.js

--- puppet.js
+++ puppet.js
@@ -16,32 +16,38 @@ const ws = wb.Sheets[wb.SheetNames[0]];
 // Converte a planilha para um array
 const data = XLSX.utils.sheet_to_json(ws, { header: 1 });
 
-// Print no console a segunda linha da primeira coluna (Linhas e colunas começam com 0)
 const ip = data[1][0]; 
 
-// Seleciona as credenciais a serem usadas ao acessar a página de configuração do dispositivo
 await page.authenticate({'username':'USUARIO', 'password': 'SENHA'});
 
 for(let i = 1;i < data.length; i++){
-    console.log(`Se conectando ao IP  ${data[i][0]}`)
+    console.log(`\nSe conectando ao IP  ${data[i][0]} ...`)
     try{
         await page.goto(`http://${data[i][0]}`, { timeout: 10000, waitUntil: 'load' })
     }
-    catch(err){
+    catch(err){ // TODO: gerencie erros de forma mais detalhada
         console.error(`ERRO: Não foi possivel acessar: ${data[i][0]}`);
         continue
     }
-        // Seleciona elemento da página com status do dispositivo, se não achar depois de 20 segundos manda um erro. 
-        const iotStatus = await page
-        .waitForSelector('.content_main',{ visible:true, timeout:20000 })   
+
+    // Seleciona elemento da página com status do dispositivo 
+    const iotStatus = await page.waitForSelector('.content_main',{ visible:true, timeout:20000 })   
+    await new Promise(r => setTimeout(r, 1000)); // Espera 1 segundo pra garantir que o elemento terminou de renderizar
+ 
 
     try{
-    // Tira um print do element com status do dispsitivo 
-    await iotStatus.screenshot({path:"screenshot-" + i + ".jpg"})
+        // Tira um print do element com status do dispsitivo e pega as informações  
+        await page.waitForSelector('#mac');
+        
+        const numSerie = await page.$eval('#mac',el => el.innerText)
+        console.log(`Número de série do equipamento: ${numSerie}`)
+
+        await iotStatus.screenshot({path:`print-${numSerie}.jpg`})
     }
     catch(err){
         console.error(`ERRO: Página de Status indisponivel`)
     } 
 }
+
 // Fecha o browser
 await browser.close()
